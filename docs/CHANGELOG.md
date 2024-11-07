9a8a43f0ef0a5ed2f7050e23200b794f1f1e2ae9
Raine-Yang-UofT <raine.yang@mail.utoronto.ca>
2024-11-07 17:36:19 -0500

    refactor: modify Portfolio constructor interface
    Modify the Poretfolio constructor to take Map<String, UserStock>
    Remove synchronization with current stock prices

--- docs/CHANGELOG.md
+++ docs/CHANGELOG.md
@@ -13,5 +13,6 @@
 - Added Use Cases 4 and 5.
 - Implemented entities for stock and stock market.
 - Implemented entities for user, portfolio, and transaction.
-- Add synchronization checks and null checks in StockMarket
+- Add synchronization checks and null checks in `StockMarket`
 - Modify `Portfolio` constructor to match the optional return value of `StockMarket.getStock`
+- Modify `Portfolio` constructor to take `Map<String, UserStock>` as parameter and remove synchronization with `StockMarket` data
--- src/main/java/entity/Portfolio.java
+++ src/main/java/entity/Portfolio.java
@@ -12,26 +12,8 @@ public class Portfolio {
     }
 
     // Constructor that initializes Portfolio with a map of tickers and their respective quantities
-    public Portfolio(Map<String, Integer> stocks) {
-        this.stocks = new HashMap<>();
-        StockMarket stockMarket = StockMarket.Instance();
-
-        stocks.forEach((ticker, quantity) -> {
-            // decompose Optional<Stock>
-            stockMarket.getStock(ticker).ifPresentOrElse(
-                stock -> {
-                    double price = stock.getPrice();
-                    if (price > 0) {
-                        this.stocks.put(ticker, new UserStock(stock, price, quantity));
-                    } else {
-                        throw new IllegalArgumentException("Invalid price for stock: " + ticker);
-                    }
-                },
-                () -> {
-                    throw new IllegalArgumentException("No stock found for ticker: " + ticker);
-                }
-            );
-        });
+    public Portfolio(Map<String, UserStock> stocks) {
+        this.stocks = new HashMap<>(stocks);
     }
 
     public double getTotalValue() {
