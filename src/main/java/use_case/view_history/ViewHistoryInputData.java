d2a64c414bd01c51c9fabc484f70eb179994f93b
AngelChen09 <110699188+AngelChen09@users.noreply.github.com>
2024-11-15 21:45:29 -0500

    reformated

--- src/main/java/use_case/view_history/ViewHistoryInputData.java
+++ src/main/java/use_case/view_history/ViewHistoryInputData.java
@@ -2,6 +2,7 @@ package use_case.view_history;
 
 /**
  * This class records the input data for the ViewHistory use case
+ *
  * @param credential the credential of the user
  */
 public record ViewHistoryInputData(
--- src/main/java/use_case/view_history/ViewHistoryOutputData.java
+++ src/main/java/use_case/view_history/ViewHistoryOutputData.java
@@ -4,6 +4,7 @@ import entity.TransactionHistory;
 
 /**
  * This class represents the output data for the ViewHistory use case.
+ *
  * @param transactionHistory the transaction history of the user
  */
 public record ViewHistoryOutputData(
