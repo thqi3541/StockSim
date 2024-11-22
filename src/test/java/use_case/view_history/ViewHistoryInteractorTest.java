f25ca69b791004cd0944670889989639ca892643
AngelChen09 <110699188+AngelChen09@users.noreply.github.com>
2024-11-22 13:21:28 -0500

    test: added ViewHistory test for execute buy case

---
+++ src/test/java/use_case/view_history/ViewHistoryInteractorTest.java
@@ -0,0 +1,66 @@
+package use_case.view_history;
+
+import entity.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import utility.exceptions.ValidationException;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class ViewHistoryInteractorTest {
+
+    private ViewHistoryDataAccessInterface dataAccess;
+    private ViewHistoryOutputBoundary outputPresenter;
+    private UserFactory userFactory;
+
+    @BeforeEach
+    void setUp(){
+        userFactory = new UserFactory();
+        dataAccess = Mockito.mock(ViewHistoryDataAccessInterface.class);
+        outputPresenter = Mockito.mock(ViewHistoryOutputBoundary.class);
+    }
+
+    @Test
+    void successTest() throws ValidationException {
+        User mockUser = userFactory.create("testUser", "password");
+
+        when(dataAccess.getUserWithCredential("dummy")).thenReturn(mockUser);
+
+        // prepare input data
+        ViewHistoryInputData inputData = new ViewHistoryInputData("dummy");
+
+        // prepare interactor
+        ViewHistoryInteractor interactor = new ViewHistoryInteractor(dataAccess, outputPresenter);
+        interactor.execute(inputData);
+
+        // check if message is prepared
+        verify(outputPresenter).prepareSuccessView(new ViewHistoryOutputData(
+                mockUser.getTransactionHistory()
+        ));
+
+        // check if user portfolio is empty
+        TransactionHistory mockUserTransactionHistory = mockUser.getTransactionHistory();
+        List<Transaction> userTransactions = new ArrayList<>();
+        assertEquals(userTransactions, mockUserTransactionHistory.getAllTransactions(), "Transaction History should be empty");
+
+        // mock execute buy transaction
+        Date timestamp = new Date(2024, 11, 22, 13, 05, 55);
+        String ticker = "XXXX";
+        Transaction mockBuyTransaction = new Transaction(timestamp, ticker, 10, 100.0, "buy");
+        mockUserTransactionHistory.addTransaction(mockBuyTransaction);
+
+        // check if user history is correct
+        userTransactions.add(mockBuyTransaction);
+        assertEquals(userTransactions, mockUserTransactionHistory.getAllTransactions(), "Transaction History should contain the mockBuyTransaction");
+
+        // TODO: add execute sell transaction test
+
+    }
+}
