da557f0d243661dc489ce5c670ba423693afcb8b
AngelChen09 <110699188+AngelChen09@users.noreply.github.com>
2024-11-23 23:27:34 -0500

    feat: rebased stock dao

--- src/main/java/data_access/StockDataAccessObject.java
+++ src/main/java/data_access/StockDataAccessObject.java
@@ -14,95 +14,151 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+/**
+ * A DataAccessObject that retrieves real time stock data
+ */
 public class StockDataAccessObject implements StockDataAccessInterface {
     private static final String BASE_URL = "https://finnhub.io/api/v1";
+    private static final String TICKERS_FILE = "/config/tickers.txt";
+
     private final OkHttpClient client;
     private final String apiKey;
+    private final List<String> tickers;
 
     public StockDataAccessObject() {
         this.client = new OkHttpClient();
+        this.tickers = new ArrayList<>();
 
-        // Load .env.local file and get the API token
+        // Load .env.local file and get API token
         Dotenv dotenv = Dotenv.configure().filename(".env.local").load();
         this.apiKey = dotenv.get("STOCK_API_KEY");
 
+        // Fetch ticker data from resource file and store in list
+        try (InputStream inputStream = getClass().getResourceAsStream(TICKERS_FILE)) {
+            if (inputStream == null) {
+                throw new RuntimeException("Unable to find configuration file: " + TICKERS_FILE);
+            } else {
+                // Reads content in config/tickers text file
+                try (Scanner scanner = new Scanner(inputStream)) {
+                    while (scanner.hasNextLine()) {
+                        String ticker = scanner.nextLine().trim();
+                        // Stores ticker in list containing all tickers
+                        tickers.add(ticker);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Error loading configuration file", e);
+        }
+
     }
 
-    public static void main(String[] args) {
-        StockDataAccessObject stockDataAccessObject = new StockDataAccessObject();
-        stockDataAccessObject.getStocks();
-    }
 
     /**
-     * Get the prices of all stocks
+     * Get all stock information
      *
      * @return a hashmap with the stock ticker as the key and the Stock entity as the value.
      * It should contain all stocks in the database.
      */
     @Override
-    public Map<String, Stock> getStocks() {
-        Map<String, Stock> stocks = new HashMap<>();
+    public Map<String, Stock> getStocks() throws RateLimitExceededException {
+        HashMap<String, Stock> stocks = new HashMap<>();
 
-        try {
-            // Reads content in config/tickers text file
-            InputStream inputStream = getClass().getResourceAsStream("/config/tickers.txt");
+        for (String ticker : tickers) {
+            // Information to create a new default stock
+            String company = "Unknown Company Name";
+            String industry = "Unknown Industry";
 
-            if (inputStream == null) {
-                throw new FileNotFoundException("Ticker resource file not found.");
-            }
-            Scanner scanner = new Scanner(inputStream);
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine().trim();
+            // Retrieves ticker current market price
+            double price = getMarketPrice(ticker);
 
-                // Information to create a new stock
-                String ticker = line;
-                String company = "Unknown Company Name";
-                String industry = "Unknown Industry";
-                double price = 0.0;
-
-                // Url and request for Quote api call
-                String quoteUrl = String.format("%s/quote?symbol=%s&token=%s", BASE_URL, ticker, apiKey);
-                Request quoteRequest = new Request.Builder().url(quoteUrl).build();
-
-                // Url and request for Profile2 api call
+            // Profile2 api call to get company name and industry
+            try {
+                // Creates new request with Profile2 url
                 String profileUrl = String.format("%s/stock/profile2?symbol=%s&token=%s", BASE_URL, ticker, apiKey);
                 Request profileRequest = new Request.Builder().url(profileUrl).build();
 
-                // Quote api call to get current market executionPrice
-                try (Response quoteResponse = client.newCall(quoteRequest).execute()) {
-                    if (quoteResponse.isSuccessful()) {
-                        String quoteResponseBody = quoteResponse.body().string();
-                        JSONObject jsonObject = new JSONObject(quoteResponseBody);
-                        price = jsonObject.getDouble("c");
-                    } else {
-                        System.out.println("API call limit exceeded.");
-                    }
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-
-                // Profile2 api call to get company name and industry
+                // Initiates API call request
                 try (Response profileResponse = client.newCall(profileRequest).execute()) {
                     if (profileResponse.isSuccessful()) {
-                        String profileResponseBody = profileResponse.body().string();
-                        JSONObject jsonObject = new JSONObject(profileResponseBody);
-                        company = jsonObject.getString("name");
-                        industry = jsonObject.getString("finnhubIndustry");
+                        JSONObject jsonObject = new JSONObject(profileResponse.body().string());
+                        // Gets the ticker company "name" or returns default company name if unavailable
+                        company = jsonObject.optString("name", company);
+                        // Gets the company's industry "finnhubIndustry" based on finnhub's classification or returns default industry if unavailable
+                        industry = jsonObject.optString("finnhubIndustry", industry);
                     } else {
-                        System.out.println("API call limit exceeded.");
+                        System.out.println("Failed to fetch profile data for ticker: " + ticker);
                     }
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
                 }
 
-                // Creates new Stock with retrieved information
-                Stock stock = new Stock(ticker, company, industry, price);
-                stocks.put(line, stock);
+                // Check if the stock already exists in the map
+                if (stocks.containsKey(ticker)) {
+                    // Update the price of the existing stock
+                    stocks.get(ticker).updatePrice(price);
+                } else {
+                    // Create a new Stock object and add it to the map
+                    Stock stock = new Stock(ticker, company, industry, price);
+                    stocks.put(ticker, stock);
+                }
+
+            } catch (Exception e) {
+                // Throws RateLimitExceededException if api call limit is exceeded
+                throw new RateLimitExceededException();
             }
-            scanner.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         }
-        return stocks;
+
+        // Return an unmodifiable view of the stocks map to prevent external modifications
+        return Collections.unmodifiableMap(stocks);
     }
+
+    /**
+     * Get updated market price for all stocks
+     *
+     * @return a hashmap with the stock ticker as the key and the updated market price as the value.
+     */
+    @Override
+    public Map<String, Double> getUpdatedPrices() throws RateLimitExceededException {
+        HashMap<String, Double> updatedPrices = new HashMap<>();
+
+        // Calls helper function to get current market price for each ticker and stores it in updatedPrice
+        for (String ticker : tickers) {
+            updatedPrices.put(ticker, getMarketPrice(ticker));
+        }
+
+        // Return an unmodifiable view of the updated prices map to prevent external modifications
+        return Collections.unmodifiableMap(updatedPrices);
+    }
+
+    /**
+     * Helper function to retrieve the current market price of the given ticker
+     *
+     * @param ticker the stock ticker
+     * @return the updated price
+     */
+    private double getMarketPrice(String ticker) throws RateLimitExceededException {
+
+        // Quote api call to get current market price
+        try {
+            // Creates new request with Quote url
+            String quoteUrl = String.format("%s/quote?symbol=%s&token=%s", BASE_URL, ticker, apiKey);
+            Request quoteRequest = new Request.Builder().url(quoteUrl).build();
+
+            // Initiates API call request
+            try (Response quoteResponse = client.newCall(quoteRequest).execute()) {
+                if (quoteResponse.isSuccessful()) {
+                    JSONObject jsonObject = new JSONObject(quoteResponse.body().string());
+                    // Gets the current market price "c"
+                    return jsonObject.getDouble("c");
+                } else {
+                    System.out.println("Failed to fetch quote data for ticker: " + ticker);
+                }
+            }
+        } catch (Exception e) {
+            // Throws RateLimitExceededException if api call limit is exceeded
+            throw new RateLimitExceededException();
+        }
+        // Returns default price if call failed
+        return 0.0;
+    }
+
 }
