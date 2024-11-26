7566e2c468509a10e16d751df0c138a740279c3d
ivorkchan <github@cy.sb>
2024-11-26 17:51:02 -0500

    refactor: reformat code

--- src/main/java/utility/MongoDBUserDocumentParser.java
+++ src/main/java/utility/MongoDBUserDocumentParser.java
@@ -9,31 +9,33 @@ import utility.exceptions.DocumentParsingException;
 /**
  * Utility class for converting between MongoDB {@link Document} objects and {@link User} entities.
  *
- * <p>It uses {@link UserMapper} for converting between the {@link User} entity and {@link UserDTO},
- * and {@link MongoDBDocumentParser} for handling the lower-level serialization and deserialization
- * between {@link Document} and the {@link UserDTO}.
+ * <p>It uses {@link UserMapper} for converting between the {@link User} entity and
+ * {@link UserDTO}, and {@link MongoDBDocumentParser} for handling the lower-level serialization and
+ * deserialization between {@link Document} and the {@link UserDTO}.
  */
 public class MongoDBUserDocumentParser {
 
-    /**
-     * Converts a MongoDB {@link Document} into a {@link User} entity.
-     *
-     * @param document the MongoDB document representing the user.
-     * @return the {@link User} entity constructed from the document.
-     * @throws DocumentParsingException if there is an error during the deserialization or mapping process.
-     */
-    public static User fromDocument(Document document) throws DocumentParsingException {
-        return UserMapper.fromDTO(MongoDBDocumentParser.fromDocument(document, UserDTO.class));
-    }
+  /**
+   * Converts a MongoDB {@link Document} into a {@link User} entity.
+   *
+   * @param document the MongoDB document representing the user.
+   * @return the {@link User} entity constructed from the document.
+   * @throws DocumentParsingException if there is an error during the deserialization or mapping
+   *                                  process.
+   */
+  public static User fromDocument(Document document) throws DocumentParsingException {
+    return UserMapper.fromDTO(MongoDBDocumentParser.fromDocument(document, UserDTO.class));
+  }
 
-    /**
-     * Converts a {@link User} entity into a MongoDB {@link Document}.
-     *
-     * @param user the {@link User} entity to be converted to a MongoDB document.
-     * @return the MongoDB {@link Document} representing the user.
-     * @throws DocumentParsingException if there is an error during the mapping or serialization process.
-     */
-    public static Document toDocument(User user) throws DocumentParsingException {
-        return MongoDBDocumentParser.toDocument(UserMapper.toDTO(user));
-    }
+  /**
+   * Converts a {@link User} entity into a MongoDB {@link Document}.
+   *
+   * @param user the {@link User} entity to be converted to a MongoDB document.
+   * @return the MongoDB {@link Document} representing the user.
+   * @throws DocumentParsingException if there is an error during the mapping or serialization
+   *                                  process.
+   */
+  public static Document toDocument(User user) throws DocumentParsingException {
+    return MongoDBDocumentParser.toDocument(UserMapper.toDTO(user));
+  }
 }
--- src/main/java/utility/data_transfer/UserMapper.java
+++ src/main/java/utility/data_transfer/UserMapper.java
@@ -1,121 +1,125 @@
 package utility.data_transfer;
 
-import entity.*;
-
+import entity.Portfolio;
+import entity.Stock;
+import entity.Transaction;
+import entity.TransactionHistory;
+import entity.User;
+import entity.UserStock;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class UserMapper {
 
-    public static UserDTO toDTO(User user) {
-        return new UserDTO(
-                user.getUsername(),
-                user.getPassword(),
-                user.getBalance(),
-                toDTO(user.getPortfolio()),
-                toDTO(user.getTransactionHistory())
-        );
-    }
+  public static UserDTO toDTO(User user) {
+    return new UserDTO(
+        user.getUsername(),
+        user.getPassword(),
+        user.getBalance(),
+        toDTO(user.getPortfolio()),
+        toDTO(user.getTransactionHistory())
+    );
+  }
 
-    public static PortfolioDTO toDTO(Portfolio portfolio) {
-        return new PortfolioDTO(portfolio.getUserStocks()
-                .entrySet()
-                .stream()
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        entry -> toDTO(entry.getValue())
-                ))
-        );
-    }
+  public static PortfolioDTO toDTO(Portfolio portfolio) {
+    return new PortfolioDTO(portfolio.getUserStocks()
+                                     .entrySet()
+                                     .stream()
+                                     .collect(Collectors.toMap(
+                                         Map.Entry::getKey,
+                                         entry -> toDTO(entry.getValue())
+                                     ))
+    );
+  }
 
-    public static UserStockDTO toDTO(UserStock userStock) {
-        return new UserStockDTO(
-                toDTO(userStock.getStock()),
-                userStock.getAvgCost(),
-                userStock.getQuantity()
-        );
-    }
+  public static UserStockDTO toDTO(UserStock userStock) {
+    return new UserStockDTO(
+        toDTO(userStock.getStock()),
+        userStock.getAvgCost(),
+        userStock.getQuantity()
+    );
+  }
 
-    public static StockDTO toDTO(Stock stock) {
-        return new StockDTO(
-                stock.getTicker(),
-                stock.getCompany(),
-                stock.getIndustry(),
-                stock.getMarketPrice()
-        );
-    }
+  public static StockDTO toDTO(Stock stock) {
+    return new StockDTO(
+        stock.getTicker(),
+        stock.getCompany(),
+        stock.getIndustry(),
+        stock.getMarketPrice()
+    );
+  }
 
-    public static TransactionDTO toDTO(Transaction transaction) {
-        return new TransactionDTO(
-                transaction.timestamp(),
-                transaction.ticker(),
-                transaction.quantity(),
-                transaction.executionPrice(),
-                transaction.type()
-        );
-    }
+  public static TransactionDTO toDTO(Transaction transaction) {
+    return new TransactionDTO(
+        transaction.timestamp(),
+        transaction.ticker(),
+        transaction.quantity(),
+        transaction.executionPrice(),
+        transaction.type()
+    );
+  }
 
-    public static TransactionHistoryDTO toDTO(TransactionHistory transactionHistory) {
-        return new TransactionHistoryDTO(transactionHistory.getTransactions()
-                .stream()
-                .map(UserMapper::toDTO)
-                .toList()
-        );
-    }
+  public static TransactionHistoryDTO toDTO(TransactionHistory transactionHistory) {
+    return new TransactionHistoryDTO(transactionHistory.getTransactions()
+                                                       .stream()
+                                                       .map(UserMapper::toDTO)
+                                                       .toList()
+    );
+  }
 
-    public static User fromDTO(UserDTO userDTO) {
-        return new User(
-                userDTO.username(),
-                userDTO.password(),
-                userDTO.balance(),
-                fromDTO(userDTO.portfolio()),
-                fromDTO(userDTO.transactionHistory())
-        );
-    }
+  public static User fromDTO(UserDTO userDTO) {
+    return new User(
+        userDTO.username(),
+        userDTO.password(),
+        userDTO.balance(),
+        fromDTO(userDTO.portfolio()),
+        fromDTO(userDTO.transactionHistory())
+    );
+  }
 
-    public static Portfolio fromDTO(PortfolioDTO portfolioDTO) {
-        return new Portfolio(portfolioDTO.userStocks()
-                .entrySet()
-                .stream()
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        entry -> fromDTO(entry.getValue())
-                ))
-        );
-    }
+  public static Portfolio fromDTO(PortfolioDTO portfolioDTO) {
+    return new Portfolio(portfolioDTO.userStocks()
+                                     .entrySet()
+                                     .stream()
+                                     .collect(Collectors.toMap(
+                                         Map.Entry::getKey,
+                                         entry -> fromDTO(entry.getValue())
+                                     ))
+    );
+  }
 
-    public static UserStock fromDTO(UserStockDTO userStockDTO) {
-        return new UserStock(
-                fromDTO(userStockDTO.stock()),
-                userStockDTO.avgCost(),
-                userStockDTO.quantity()
-        );
-    }
+  public static UserStock fromDTO(UserStockDTO userStockDTO) {
+    return new UserStock(
+        fromDTO(userStockDTO.stock()),
+        userStockDTO.avgCost(),
+        userStockDTO.quantity()
+    );
+  }
 
-    public static Stock fromDTO(StockDTO stockDTO) {
-        return new Stock(
-                stockDTO.ticker(),
-                stockDTO.company(),
-                stockDTO.industry(),
-                stockDTO.marketPrice()
-        );
-    }
+  public static Stock fromDTO(StockDTO stockDTO) {
+    return new Stock(
+        stockDTO.ticker(),
+        stockDTO.company(),
+        stockDTO.industry(),
+        stockDTO.marketPrice()
+    );
+  }
 
-    public static Transaction fromDTO(TransactionDTO transactionDTO) {
-        return new Transaction(
-                transactionDTO.timestamp(),
-                transactionDTO.ticker(),
-                transactionDTO.quantity(),
-                transactionDTO.executionPrice(),
-                transactionDTO.type()
-        );
-    }
+  public static Transaction fromDTO(TransactionDTO transactionDTO) {
+    return new Transaction(
+        transactionDTO.timestamp(),
+        transactionDTO.ticker(),
+        transactionDTO.quantity(),
+        transactionDTO.executionPrice(),
+        transactionDTO.type()
+    );
+  }
 
-    public static TransactionHistory fromDTO(TransactionHistoryDTO transactionHistoryDTO) {
-        return new TransactionHistory(transactionHistoryDTO.transactions()
-                .stream()
-                .map(UserMapper::fromDTO)
-                .toList()
-        );
-    }
+  public static TransactionHistory fromDTO(TransactionHistoryDTO transactionHistoryDTO) {
+    return new TransactionHistory(transactionHistoryDTO.transactions()
+                                                       .stream()
+                                                       .map(UserMapper::fromDTO)
+                                                       .toList()
+    );
+  }
 }
